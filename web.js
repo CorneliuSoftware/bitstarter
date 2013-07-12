a33efbe620f5f0a5d80d5fa5fb001c369827eb54
Corneliu Software <corneliu.software@gmail.com>
2013-07-12 05:03:47 +0000

    change

--- web.js
+++ web.js
@@ -2,10 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var fileName = './index.html';
 var textString = fs.readFileSync(fileName,'utf8');
+var t2= "Hello 2";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(textString);
+  response.send(t2);
 });
 
 var port = process.env.PORT || 5000;
